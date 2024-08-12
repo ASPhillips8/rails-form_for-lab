313a9f1a8d506d6e0f3545fe565144ad49c2fbe3
Anthony Phillips <anthony.s.phillips888@gmail.com>
2024-08-12 15:36:38 -0400

    Create views and update update action

--- app/controllers/school_classes_controller.rb
+++ app/controllers/school_classes_controller.rb
@@ -14,10 +14,13 @@ class SchoolClassesController < ApplicationController
   end
 
   def edit
-    @school_class = SchoolClass.new
+    @school_class = SchoolClass.find(params[:id])
   end
 
   def update
+    @school_class = SchoolClass.find(params[:id])
+    @school_class.update(school_class_params)
+    redirect_to school_class_path(@school_class)
   end
 
   private
