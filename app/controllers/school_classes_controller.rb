e977331868015527984c540ec21163deee1b4f4a
Anthony Phillips <anthony.s.phillips888@gmail.com>
2024-08-12 15:14:55 -0400

    Create action for school_classes

--- app/controllers/school_classes_controller.rb
+++ app/controllers/school_classes_controller.rb
@@ -1,2 +1,28 @@
 class SchoolClassesController < ApplicationController
+
+  def show
+    @school_class = SchoolClass.find(params[:id])
+  end
+
+  def new
+    @school_class = SchoolClass.new
+  end
+
+  def create
+    @school_class = SchoolClass.create(school_class_params)
+    redirect_to school_class_path(@school_class)
+  end
+
+  def edit
+    @school_class = SchoolClass.new
+  end
+
+  def update
+  end
+
+  private
+
+  def school_class_params
+    params.require(:school_class).permit(:title, :room_number)
+  end
 end
